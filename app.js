299f35da618c7472a1e371ee357c60cb120c18e8
Bryce Murray <brycepmurray711@gmail.com>
2018-02-05 15:48:31 -0700

    functionality minus attack and defense mods

--- app.js
+++ app.js
@@ -1,82 +1,93 @@
-var Player = (health, items, attacks, hits) => {
-    this.helath = health
+function gameManager(slap, elCangrejoSubmission, anacondaSqueeze) {
+
+}
+
+var Player = function(health, attacks, hits) {
+    this.health = health;
     this.items = [];
-    this.attacks = attacks
+    this.attacks = attacks;
     this.hits = hits
 }
 
-var Item = (name, mod, description) => {
-    this.name = name
-    this.mod = mod
+var Item = function(name, mod, description) {
+    this.name = name;
+    this.mod = mod;
     this.description = description
 }
 
-var Nacho = new Player(100, [], [slap, elCangrejoSubmission, anacondaSqueeze], 0);
+var nacho = new Player(100, [slap, elCangrejoSubmission, anacondaSqueeze], 0);
 
 var eaglePowers = new Item("Eagle Powers", 2, "Nacho Summons His Eagle Powers!")
 var stretchyPants = new Item("Stretchy Pants", 2, "Nacho Wears His Stretchy Pants!")
 var corn = new Item("Delicious Corn", 15, "Nacho Enjoys His Deliciuos Corn!")
 
-Nacho.items.push(eaglePowers, stretchyPants, corn)
+// Nacho.items.push(eaglePowers, stretchyPants, corn)
 
-console.log(Nacho)
+console.log(nacho)
 
-var Ramses = new Player(100, [], [slap, elCangrejoSubmission, anacondaSqueeze], 0);
+var ramses = new Player(100, [slap, elCangrejoSubmission, anacondaSqueeze], 0);
 
 var fancyOils = new Item("Facy Oils", 2, "Ramses Uses The Fancy Oils To Be Slick!")
 var championsBelt = new Item("Champions Belt", 15, "Ramses Feels Revitalized Remembering that Ramses is Number Juan")
 var chair = new Item("The Chair", 2, "Ramses is Handed The Chair From Under The Stage")
 
-Ramses.items.push(chair, championsBelt, fancyOils)
-
-console.log(Ramses)
+// Ramses.items.push(chair, championsBelt, fancyOils)
 
 var attacks = [slap, elCangrejoSubmission, anacondaSqueeze];
 
-function slap(Nacho, Ramses) {
-    Ramses.helath -= 1 * item.mod();
-    Ramses.hits += 1
-    if (Ramses.health < 0) {
-        Ramses.health = 0
+function slap(ramses) {
+    ramses.health -= 1;
+    ramses.hits += 1
+    if (ramses.health < 0) {
+        ramses.health = 0
     }
     update()
 }
 
-function elCangrejoSubmission(Nacho, Ramses) {
-    Ramses.helath -= 5 * item.mod();
-    Ramses.hits += 1
-    if (Ramses.health < 0) {
-        Ramses.health = 0
+function elCangrejoSubmission(ramses) {
+    ramses.health -= 5;
+    ramses.hits += 1
+    if (ramses.health < 0) {
+        ramses.health = 0
     }
     update()
 }
 
-function anacondaSqueeze(Nacho, Ramses) {
-    Ramses.helath -= 10 * item.mod();
-    Ramses.hits += 1
-    if (Ramses.health < 0) {
-        Ramses.health = 0
+function anacondaSqueeze(ramses) {
+    ramses.health -= 10;
+    ramses.hits += 1
+    if (ramses.health < 0) {
+        ramses.health = 0
     }
     update()
 }
 
-function RamsesChoices(min, max) {
-    min = Math.ceil(1);
-    max = Math.floor(11);
-    return Math.floor(Math.random() * (11 - 1)) + 1;
-
+function itemPowers() {
+    var total = 1
+    for (let i = 0; i < nacho.items.length; i++) {
+        const item = nacho.items[i];
+        total += item.mod
+    }
+    return total
 }
+
+// function ramsesChoices(min, max) {
+//     min = Math.ceil(1);
+//     max = Math.floor(11);
+//     return Math.floor(Math.random() * (11 - 1)) + 1;
+
+// }
 
 function update() {
-    document.getElementById("Nacho.health").innerText = Nacho.health;
-    document.getElementById("Nacho.hits").innerText = Nacho.hits;
-    document.getElementById("Ramses.health").innerText = Ramses.health;
-    document.getElementById("Ramses.hits").innerText = Ramses.hits
-    if (Nacho.health <= 0) {
-        document.getElementsByClassName("buttons").innerHTML = "<h2> Im Sick Of Losing, I Wanna Win!! </h2>"
+    document.getElementById("nacho.health").innerText = nacho.health;
+    document.getElementById("nacho.hits").innerText = nacho.hits;
+    document.getElementById("ramses.health").innerText = ramses.health;
+    document.getElementById("ramses.hits").innerText = ramses.hits
+    if (nacho.health <= 0) {
+        document.getElementsByClassName("btn-defaut").innerHTML = "<h2> Im Sick Of Losing, I Wanna Win!! </h2>"
     }
-    if (Ramses.health <= 0) {
-        document.getElementsByClassName("buttons").innerHTML = "<h2> Its the Best!! </h2>"
+    if (ramses.health <= 0) {
+        document.getElementsByClassName("btn-default").innerHTML = "<h2> Its the Best!! </h2>"
     }
 }
 update()
